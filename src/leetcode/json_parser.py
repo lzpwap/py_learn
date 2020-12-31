c3c973deec360e46723803e4291640477ae09291
lizhipei <lizhipei@shein.com>
2020-12-31 22:44:55 +0800

    json解析开始

--- src/leetcode/json_parser.py
+++ src/leetcode/json_parser.py
@@ -3,27 +3,90 @@
 
 __author__ = "lizhipei"
 
+
 class JsonFormatException(Exception):
     __name__ = "JsonFormatException"
-    
 
-def jsonStringToDic(jsonStr:str):
-    stack = list()
-    count = 0;
-    if len(jsonStr)==0:
+
+class Symbol():
+    __slots__ = ("_symbol", "_index")
+
+    def __init__(self, symbol, index) -> None:
+        self.setSymbol(symbol)
+        self.setIndex(index)
+
+    def setSymbol(self, value):
+        self._symbol = value
+
+    def getSymbol(self):
+        return self._symbol
+
+    def setIndex(self, index):
+        self.index = index
+
+    def getIndex(self):
+        return self._index
+
+    def isObjEnd(self):
+        return self._symbol == '}'
+
+    def isObjStart(self):
+        return self._symbol == '{'
+
+    def isArrStart(self):
+        return self._symbol == '['
+
+    def isArrEnd(self):
+        return self._symbol == ']'
+
+
+def isObjEnd(c: str):
+    return c == '}'
+
+
+def isObjStart(c: str):
+    return c == '{'
+
+
+def isArrStart(c: str):
+    return c == '['
+
+
+def isArrEnd(c: str):
+    return c == ']'
+
+
+def jsonStringToDic(jsonStr: str):
+    if len(jsonStr) == 0:
         return dict()
-    if jsonStr[0] != '[' and jsonStr[0] !='{':
+    if not isObjStart(jsonStr[0]) and not isArrStart(jsonStr[0]):
         raise JsonFormatException("json format invalid")
-    for i in jsonStr:
-        if i=='{':
-            stack.append(i)
-        if i=='}':
-            if stack.pop()=='{':
-                count+=1
-            else:
-                stack.append('{')
+
+    stack = list()
+    index = 0
+    while index < len(jsonStr):
+        char = jsonStr[index]
+        if isObjStart(char):
+            stack.append(Symbol(char, index))
+        if isArrStart(char):
+            stack.append(Symbol(char, index))
+        index += 1
     jsonMap = dict()
     return jsonMap
+
+
+def parseObj(obj: str):
+    return dict()
+
+
+def nextObjectStartSymbol(currIndex, jsonStr: str):
+    index = currIndex+1
+    while index < len(jsonStr):
+        if isObjEnd(jsonStr[index]):
+            return index
+        index += 1
+    return -1
+
 
 try:
     jsonStringToDic("1")
